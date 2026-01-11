74e6f9db708508b3b0dfc7225bbae0669b880b54
Daniel Allen <dan@aeternagroup.net>
2026-01-11 17:56:56 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,58 @@
+variable "recovery_services_vaults" {
+  description = <<EOT
+Map of recovery_services_vaults, attributes below
+Required:
+    - location
+    - name
+    - resource_group_name
+    - sku
+Optional:
+    - classic_vmware_replication_enabled
+    - cross_region_restore_enabled
+    - immutability
+    - public_network_access_enabled
+    - soft_delete_enabled
+    - storage_mode_type
+    - tags
+    - encryption (block):
+        - infrastructure_encryption_enabled (required)
+        - key_id (required)
+        - use_system_assigned_identity (optional)
+        - user_assigned_identity_id (optional)
+    - identity (block):
+        - identity_ids (optional)
+        - type (required)
+    - monitoring (block):
+        - alerts_for_all_job_failures_enabled (optional)
+        - alerts_for_critical_operation_failures_enabled (optional)
+EOT
+
+  type = map(object({
+    location                           = string
+    name                               = string
+    resource_group_name                = string
+    sku                                = string
+    classic_vmware_replication_enabled = optional(bool)
+    cross_region_restore_enabled       = optional(bool, false)
+    immutability                       = optional(string)
+    public_network_access_enabled      = optional(bool, true)
+    soft_delete_enabled                = optional(bool, true)
+    storage_mode_type                  = optional(string, "GeoRedundant")
+    tags                               = optional(map(string))
+    encryption = optional(object({
+      infrastructure_encryption_enabled = bool
+      key_id                            = string
+      use_system_assigned_identity      = optional(bool, true)
+      user_assigned_identity_id         = optional(string)
+    }))
+    identity = optional(object({
+      identity_ids = optional(set(string))
+      type         = string
+    }))
+    monitoring = optional(object({
+      alerts_for_all_job_failures_enabled            = optional(bool, true)
+      alerts_for_critical_operation_failures_enabled = optional(bool, true)
+    }))
+  }))
+}
+
